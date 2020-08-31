673b6f12fc2a772f48675fec6921a32820103f70
John Farrell <johnfarrell@fastmail.com>
2020-08-31 22:21:05 +0100

    improve skills section grammar

--- src/components/constants/skills.tsx
+++ src/components/constants/skills.tsx
@@ -7,7 +7,7 @@ export default [
     icon: <SiJavascript className="service-icon" />,
     title: 'JavaScript',
     description:
-      'JavaScript is a language I have been using professionally for over a year, I love it because I love the internet ' +
+      'JavaScript is a language I have been using professionally for over a year, I love it because I love the internet.' +
       'I love the internet because it is the most user friendly way to share information or build applications.'
   },
   {
@@ -23,15 +23,14 @@ export default [
     icon: <SiReact className="service-icon" />,
     title: 'React',
     description:
-      'React is my current favourite web framework (I have used Angular and Vue professionally), modern functional React ' +
-      'makes reactive web-development a lot less painful. This site is built using Gatsby which is a framework built on top of React.'
+      'React is my current favourite web framework (I have used Angular and Vue professionally), I find modern functional React makes ' +
+      'reactive web-development a lot less painful. I built this site using Gatsby which is a framework built on top of React.'
   },
   {
     id: 4,
     icon: <SiJava className="service-icon" />,
     title: 'Java',
-    description:
-      'I find Java great for large scale enterprise development. I am interested in alternatives though. (Python, Go, C, C++, C#)'
+    description: 'I find Java great for large scale enterprise development. I am interested in alternatives though (Python, Go, C, C++, C#)'
   },
   {
     id: 5,
@@ -39,6 +38,6 @@ export default [
     title: 'Spring',
     description:
       "I've used Spring whilst working on professional web applications, Spring made the backend/server-side development " +
-      "simple with it's many pre-configurations."
+      'simple with its many pre-configurations.'
   }
 ]
