8cb9e58ce3ba6e8c7977fc4bebf09b3c4f254fad
John Farrell <johnkevinfarrell93@gmail.com>
2020-09-01 23:40:45 +0100

    attempt favicon

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 import { useStaticQuery, graphql } from 'gatsby'
+import favicon from '../static/favicon.ico'
 
 const query = graphql`
   {
@@ -27,6 +28,8 @@ interface SEOProps {
 }
 
 const SEO = ({ title, description }: SEOProps) => {
+  console.log('favicon', favicon)
+
   const { site } = useStaticQuery(query)
 
   const { siteDescription, siteTitle, siteUrl, image, twitterUsername } = site.siteMetadata
@@ -45,6 +48,13 @@ const SEO = ({ title, description }: SEOProps) => {
       <meta name="twitter:title" content={siteTitle} />
       <meta name="twitter:description" content={siteDescription} />
       <meta name="twitter:image" content={`${siteUrl}${image}`} />
+
+      <link rel="apple-touch-icon" sizes="180x180" href={favicon} />
+      <link rel="icon" type="image/png" sizes="32x32" href={favicon} />
+      <link rel="icon" type="image/png" sizes="16x16" href={favicon} />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
     </Helmet>
   )
 }
