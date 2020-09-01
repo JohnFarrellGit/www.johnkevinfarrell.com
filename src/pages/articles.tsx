c747f579abd40ea0eaca52dbf89ccb21b1ac96e7
John Farrell <johnfarrell@fastmail.com>
2020-09-01 22:39:02 +0100

    implement SEO component into every site page

--- src/pages/articles.tsx
+++ src/pages/articles.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import ArticlesComponent from '../components/Articles'
+import SEO from '../components/SEO'
 
 const ArticlesPage = () => {
   return (
     <Layout>
+      <SEO title="Articles" description="Articles written by John Farrell" />
       <section className="blog-page">
         <ArticlesComponent />
       </section>
