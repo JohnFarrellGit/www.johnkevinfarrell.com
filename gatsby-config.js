6f460513463d90eb8e5ee8d0e5fddb171d977ce5
John Farrell <johnfarrell@fastmail.com>
2020-09-01 21:38:11 +0100

    update canonical URL

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,7 +43,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-canonical-urls',
       options: {
-        siteUrl: 'https://gatsby-starter-typescript-plus.netlify.com'
+        siteUrl: 'https://www.johnfarrell.dev/'
       }
     },
     'gatsby-plugin-emotion',
